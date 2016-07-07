a814bbe16521c31117ccfbe1d9d6a677a1e08c80
brandonxiang <1542453460@qq.com>
2016-07-07 17:25:12 +0800

    test for test.json

--- index.js
+++ index.js
@@ -9,41 +9,52 @@ var map = new L.Map('map', {
     zoom: 9
 });
 
-// var one, two, three, points;
-// $.get("1.json", function (data) {
-//     one = L.geoJson(data);
-//     $.get("2.json", function (data) {
-//         two = L.geoJson(data);
-//         $.get("3.json", function (data) {
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
 
-//             three = L.geoJson(data);
-
-//         })
-
-//     })
-// })
-
-// $.get("test.json", function (data) {
-//     test = L.geoJson(data);
-
-//     var sliderControl = L.control.sliderControl({
-//         layer: test,
-//         range: false
-//     });
-//     map.addControl(sliderControl);
-//     sliderControl.startSlider();
-// });
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
 
 
+$.get("test.json", function (data) {
+    test = L.geoJson(data,{style:style});
 
-$.get("points.json", function (data) {
-    points = L.geoJson(data);
- var sliderControl = L.control.sliderControl({
-        layer: points,
-        range: false
+    var sliderControl = L.control.sliderControl({
+        layer: test,
+        range: false,
+        follow:1
     });
     map.addControl(sliderControl);
     sliderControl.startSlider();
-})
+});
+
+
+
+// $.get("points.json", function (data) {
+//     points = L.geoJson(data);
+//  var sliderControl = L.control.sliderControl({
+//         layer: points,
+//         range: false,
+//         follow:1
+//     });
+//     map.addControl(sliderControl);
+//     sliderControl.startSlider();
+// })
 
 // var layerGroup = L.layerGroup([one,two,three]);
